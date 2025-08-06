76c9aaf3cc580b3d53f1c55b7186c31ead80ca95
Jared Buckley <jared@Jareds-Mac-Studio.local>
2025-08-06 16:54:36 +0200

    Fix dependency conflicts and update to compatible versions

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,10 +7,8 @@ const config: StorybookConfig = {
     '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'
   ],
   addons: [
-    '@storybook/addon-onboarding',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@chromatic-com/storybook',
     '@storybook/addon-interactions',
   ],
   framework: {
@@ -26,8 +24,8 @@ const config: StorybookConfig = {
       propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
     },
   },
-  features: {
-    experimentalRSC: true,
+  docs: {
+    autodocs: 'tag',
   },
 }
 
